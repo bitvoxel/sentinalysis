500d5651fda732bb37f1dee434bf5c358cb09ff6
Vignesh R <sskzm6095@gmail.com>
2024-10-18 14:32:25 +0000

    changed vectoriser

--- senti/backend/sentiment.py
+++ senti/backend/sentiment.py
@@ -1,52 +1,54 @@
-# import pandas as pd
-# from sklearn.feature_extraction.text import re, TfidfVectorizer
-# from sklearn.model_selection import train_test_split
-# from sklearn.linear_model import LogisticRegression
-# from sklearn.metrics import accuracy_score
-# import numpy as np
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import re
+import nltk
+from nltk.corpus import stopwords
 
-# df = pd.read_csv('/workspaces/codespaces-blank/senti/backend/chat.csv')
-# def create_sentiment(rating):
-    
-#     if rating=='negative':
-#         return -1 # negative sentiment
-#     elif rating=='positive':
-#         return 1 # positive sentiment
-#     else:
-#         return 0 # neutral sentiment
-    
+nltk.download("stopwords")
 
 
-# def clean_data(review):
-#     no_punc = re.sub(r'[^\w\s]', '', review)
-#     no_digits = ''.join([i for i in no_punc if not i.isdigit()])
-#     return(no_digits)
+def create_sentiment(rating):
 
-# def processData(string):
-#     Dataframe=pd.DataFrame([['message'],[string]])
-#     TFI_VECTOR = tfidf.fit_transform(df['message'])
-#     return TFI_VECTOR
-
-
-# df['sentiment'] = df['sentiment'].apply(create_sentiment)
-# df['message']=df['message'].apply(clean_data)
-# tfidf = TfidfVectorizer(strip_accents=None, 
-#                         lowercase=False,
-#                         preprocessor=None)
-# TFI_VECTOR = tfidf.fit_transform(df['message'])
-# TARGET = df['sentiment']
-# X_train, X_test, y_train, y_test = train_test_split(TFI_VECTOR,TARGET)
-# LogReg = LogisticRegression(solver='sag')
-# LogReg.fit(X_train,y_train) # fit the model
+    if rating == "negative":
+        return -1  # negative sentiment
+    elif rating == "positive":
+        return 1  # positive sentiment
+    else:
+        return 0  # neutral sentiment
 
 
-# preds = LogReg.predict(X_test) # make predictions
-# print(preds.tolist())
-# print(y_test)
+def clean_data(review):
+    no_punc = re.sub(r"[^\w\s]", "", review)
+    no_digits = "".join([i for i in no_punc if not i.isdigit()])
+    return no_digits
 
-# Use a pipeline as a high-level helper
-# Use a pipeline as a high-level helper
-from transformers import pipeline
-def classify(text):
-    pipe = pipeline("text-classification", model="j-hartmann/sentiment-roberta-large-english-3-classes")
-    return pipe(text)
+
+def model(df=pd.read_csv("/workspaces/codespaces-blank/senti/backend/chat.csv")):
+    df["sentiment"] = df["sentiment"].apply(create_sentiment)
+    df["message"] = df["message"].apply(clean_data)
+    vectorizer = CountVectorizer(stop_words=stopwords.words("english"))
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        df["message"],
+        df["sentiment"],
+        test_size=0.25,
+        random_state=42
+    )
+    X_train_vec = vectorizer.fit_transform(X_train)
+    X_test_vec = vectorizer.transform(X_test)
+
+    model = LogisticRegression()
+    model.fit(X_train_vec, y_train)
+    predictions = model.predict(X_test_vec)
+    print("Accuracy", accuracy_score(y_test, predictions))
+
+    new_reviews = [input("Enter sentence:")]
+    new_reviews_vec = vectorizer.transform(new_reviews)
+    predictions = model.predict(new_reviews_vec)
+    print(predictions)
+
+model()
--- senti/frontend/app.py
+++ senti/frontend/app.py
@@ -1,8 +1,7 @@
 from flask import Flask,render_template
-from backend.sentiment import classify
 
 app = Flask(__name__)
 
 @app.route('/senti/<message>')
 def hello(message=None):
-    return render_template('hello.html', person=classify(message)[0]["label"])
+    return render_template('hello.html', person=message)
